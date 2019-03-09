18e388a52ba32cc0385294073cea2fcf2c005e90
user1 <acdc@plaz.com.ua>
2019-03-09 20:51:52 +0200

    2

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -8,58 +8,86 @@ namespace ConsoleApp8
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            const int n = 3;
+            int[,] a = new int[n, n];
+
+            Console.WriteLine(diag(a, n));
+
+        }
+        /*
+        Random r = new Random();
+        const int m = 5, n = 4;
+
+
+
+        int[,] a = new int[m, n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                a[i, j] = r.Next(100);
+                Console.Write(a[i, j] + "\t");
+
+
+            }
+            Console.WriteLine();
+
+        }
+
+        int min = a[0, 0], minI = 0, minj = 0;
+        int max = a[0, 0], maxI = 0, maxj = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (min > a[i, j])
+                {
+                    min = a[i, j];
+                    minI = i;
+                    minj = j;
+                }
+
+                if (max < a[i, j])
+                {
+                    max = a[i, j];
+                    maxI = i;
+                    maxj = j;
+                }
+            }
+        }
+        Console.WriteLine($"Minimum a[{minI+1},{minj+1}] = {min}");
+        Console.WriteLine($"Maximum a[{maxI + 1},{maxj + 1}] = {max}");
+        */
+
+        public static int diag(int[,] a, int n)
+        {
+            int sum = 0;
+
             Random r = new Random();
-            const int m = 5, n = 4;
 
 
-
-            int[,] a = new int[m, n];
-            for (int i = 0; i < m; i++)
+            for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
                     a[i, j] = r.Next(100);
                     Console.Write(a[i, j] + "\t");
 
-                    
+
                 }
                 Console.WriteLine();
-
             }
 
-            int min = a[0, 0], minI = 0, minj = 0;
-            int max = a[0, 0], maxI = 0, maxj = 0;
-            for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
             {
-                for (int j = 0; j < n; j++)
-                {
-                    if (min > a[i, j])
-                    {
-                        min = a[i, j];
-                        minI = i;
-                        minj = j;
-                    }
-
-                    if (max < a[i, j])
-                    {
-                        max = a[i, j];
-                        maxI = i;
-                        maxj = j;
-                    }
-                }
+                sum += a[j, j];
             }
-            Console.WriteLine($"a[{minI+1},{minj+1}] = {min}");
-            Console.WriteLine($"a[{maxI + 1},{maxj + 1}] = {max}");
 
 
-
-
+                return sum;
 
         }
     }
-
-
-
 }
